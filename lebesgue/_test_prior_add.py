678d4b5333bfa35b2ce0351ea4cbe6ab6d9517e9
Rupert Tombs <rupert.tombs@gmail.com>
2022-05-04 23:13:39 +0100

    simplify imports

--- lebesgue/_test_prior_add.py
+++ lebesgue/_test_prior_add.py
@@ -1,10 +1,12 @@
 import numpy
 
-from . import _likelihood_poisson, _prior_add, _prior_normal, _testing
+from . import _testing
+from .likelihood import poisson
+from .prior import add, log_normal
 
 
 def test_shift():
-    prior = _prior_normal.log_normal(1, 3)
+    prior = log_normal(1, 3)
 
     rng = numpy.random.Generator(numpy.random.Philox(4))
 
@@ -13,18 +15,16 @@ def test_shift():
 
         # such that lo + x == lo_previous
         ref = prior.between(lo - x, hi - x)
-        chk = _prior_add.add(x, prior).between(lo, hi)
+        chk = add(x, prior).between(lo, hi)
 
         assert ref == chk, (ref, chk)
 
 
 def test_args():
-    prior = _prior_normal.log_normal(-4, 2.0)
-    assert _testing.raises(lambda: _prior_add.add(None, prior), TypeError)
+    prior = log_normal(-4, 2.0)
+    assert _testing.raises(lambda: add(None, prior), TypeError)
 
-    not_prior = _likelihood_poisson.poisson(3)
-    assert _testing.raises(
-        lambda: _prior_add.add(3, not_prior), AttributeError
-    )
+    not_prior = poisson(3)
+    assert _testing.raises(lambda: add(3, not_prior), AttributeError)
 
-    assert not _testing.raises(lambda: _prior_add.add(0.0, prior))
+    assert not _testing.raises(lambda: add(0.0, prior))
--- lebesgue/_test_prior_normal.py
+++ lebesgue/_test_prior_normal.py
@@ -2,6 +2,7 @@ import numpy
 import scipy.special
 
 from . import _prior_normal, _testing
+from .prior import log_normal, normal
 
 
 def test_gaussian_dcdf():
@@ -30,30 +31,26 @@ def test_between():
         ref = gaussian_dcdf_ref((x - mu) / sigma, (y - mu) / sigma)
 
         # normal
-        prior = _prior_normal.normal(mu, sigma)
+        prior = normal(mu, sigma)
         chk = prior.between(x, y)
         numpy.testing.assert_allclose(chk, ref, atol=1e-15)
 
         # log_normal
-        log_prior = _prior_normal.log_normal(mu, sigma)
+        log_prior = log_normal(mu, sigma)
         chk_log = log_prior.between(numpy.exp(x), numpy.exp(y))
         numpy.testing.assert_allclose(chk_log, ref, atol=1e-15)
 
 
 def test_args():
-    assert _testing.raises(lambda: _prior_normal.normal(None, 1), TypeError)
-    assert _testing.raises(lambda: _prior_normal.normal(1, None), TypeError)
-    assert _testing.raises(lambda: _prior_normal.normal(2, -1), ValueError)
-    assert not _testing.raises(lambda: _prior_normal.normal(3, 0.1))
+    assert _testing.raises(lambda: normal(None, 1), TypeError)
+    assert _testing.raises(lambda: normal(1, None), TypeError)
+    assert _testing.raises(lambda: normal(2, -1), ValueError)
+    assert not _testing.raises(lambda: normal(3, 0.1))
 
-    assert _testing.raises(
-        lambda: _prior_normal.log_normal(None, 1), TypeError
-    )
-    assert _testing.raises(
-        lambda: _prior_normal.log_normal(1, None), TypeError
-    )
-    assert _testing.raises(lambda: _prior_normal.log_normal(2, -1), ValueError)
-    assert not _testing.raises(lambda: _prior_normal.log_normal(3, 0.1))
+    assert _testing.raises(lambda: log_normal(None, 1), TypeError)
+    assert _testing.raises(lambda: log_normal(1, None), TypeError)
+    assert _testing.raises(lambda: log_normal(2, -1), ValueError)
+    assert not _testing.raises(lambda: log_normal(3, 0.1))
 
 
 # utilities

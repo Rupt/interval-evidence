61ed01b5d081574b0cc24e20f4acb43b7904b674
Rupert Tombs <rupert.tombs@gmail.com>
2022-05-04 21:41:44 +0100

    remove numpy dependence for caching

--- lebesgue/_prepare_caches.py
+++ lebesgue/_prepare_caches.py
@@ -5,7 +5,6 @@ The odd layout here enables numba's caching.
 
 """
 import numba
-import numpy
 
 from . import _quad_bound
 from ._bayes import Model
@@ -27,7 +26,7 @@ _quad_bound._integrator_cache[model.mass_func] = poisson_add_log_normal
 
 
 # poisson | add trunc normal
-model = Model(poisson(0), add(1, trunc(0, numpy.inf, normal(0, 1))))
+model = Model(poisson(0), add(1, trunc(0, 1, normal(0, 1))))
 
 poisson_add_trunc_normal_integrate = model.integrate_func
 

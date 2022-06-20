f610af2fcc25f0c651a8b1bf38832c4c83c39b3d
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-20 23:14:45 +0100

    add llp muon regions

---
+++ searches/ins1788448/dump_regions.py
@@ -0,0 +1,52 @@
+"""
+Usage:
+
+python searches/ins1788448/dump_regions.py
+
+"""
+
+import os
+import glob
+
+import pyhf
+
+from pyhf_stuff import region, serial
+
+BASEPATH = os.path.dirname(__file__)
+
+
+def main():
+    for name, workspace in generate_regions():
+        region.Region(name, workspace).dump(
+            os.path.join(BASEPATH, region.strip_cuts(name)),
+        )
+
+
+def generate_regions():
+    paths = glob.glob(os.path.join(BASEPATH, "*_bkg.json.gz"))
+
+    for path in paths:
+        spec = serial.load_json_gz(path)
+        workspace = pyhf.workspace.Workspace(region.clear_poi(spec))
+
+        channels = workspace.channel_slices.keys()
+
+        # find control and signals region strings
+        control_regions = set()
+        signal_regions = set()
+        for name in channels:
+            if not name.startswith("SR"):
+                control_regions.add(name)
+                continue
+
+            assert name.startswith("SR")
+            signal_regions.add(name)
+
+        name, = signal_regions
+
+        # serialize regions for each
+        yield name, region.prune(workspace, name, *control_regions)
+
+
+if __name__ == "__main__":
+    main()

144ec64f1fe737cb9da882d70660ad50d541e7b1
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-20 22:34:09 +0100

    add atlas susy 4l regions

---
+++ searches/ins1831992/dump_regions.py
@@ -0,0 +1,50 @@
+"""
+Usage:
+
+python searches/ins1831992/dump_regions.py
+
+"""
+
+import os
+
+import pyhf
+
+from pyhf_stuff import region, serial
+
+BASEPATH = os.path.dirname(__file__)
+HEPDATA = "ins1831992"
+
+
+def main():
+    for name, workspace in generate_regions():
+        region.Region(name, workspace).dump(
+            os.path.join(BASEPATH, region.strip_cuts(name)),
+        )
+
+
+def generate_regions():
+    spec = serial.load_json_gz(
+        os.path.join(BASEPATH, HEPDATA + "_bkg.json.gz")
+    )
+    workspace = pyhf.workspace.Workspace(region.clear_poi(spec))
+
+    channels = workspace.channel_slices.keys()
+
+    # find control and signals region strings
+    control_regions = set()
+    signal_regions = set()
+    for name in channels:
+        if name.startswith("CR"):
+            control_regions.add(name)
+            continue
+
+        assert name.startswith("SR")
+        signal_regions.add(name)
+
+    # serialize regions for each
+    for name in sorted(signal_regions):
+        yield name, region.prune(workspace, name, *control_regions)
+
+
+if __name__ == "__main__":
+    main()

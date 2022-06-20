7343543a28964af545a4cd70ee8885f561969db7
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-20 15:10:59 +0100

    begin interfacing fits into lebesgue for limit scans

---
+++ pyhf_stuff/models.py
@@ -0,0 +1,48 @@
+"""Construct partial lebesgue models from fit results."""
+from functools import partial
+
+import numpy
+
+import lebesgue
+
+
+def linspace(fit):
+    return partial(
+        lebesgue.canned.gamma1_regular_linear,
+        start=fit.start,
+        stop=fit.stop,
+        log_rates=numpy.negative(fit.levels),
+    )
+
+
+def mcmc(fit):
+    return partial(
+        lebesgue.canned.gamma1_regular_uniform,
+        start=fit.range_[0],
+        stop=fit.range_[1],
+        log_rates=numpy.log(fit.yields),
+    )
+
+
+def cabinetry(fit):
+    return partial(
+        lebesgue.canned.gamma1_trunc_normal,
+        mu=fit.yield_pre,
+        sigma=fit.error_pre,
+    )
+
+
+def normal(fit):
+    return partial(
+        lebesgue.canned.gamma1_trunc_normal,
+        mu=fit.yield_linear,
+        sigma=fit.error_linear,
+    )
+
+
+def normal_log(fit):
+    return partial(
+        lebesgue.canned.gamma1_log_normal,
+        mu=numpy.log(fit.yield_linear),
+        sigma=fit.error_log,
+    )

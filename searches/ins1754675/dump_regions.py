37d9dc727235532936d9da8cc1d6c93fe162ff4a
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-21 13:59:06 +0100

    ss3l+jets search

---
+++ searches/ins1754675/dump_regions.py
@@ -0,0 +1,49 @@
+"""
+Usage:
+
+python searches/ins1754675/dump_regions.py
+
+"""
+
+import os
+
+import pyhf
+
+from pyhf_stuff import region, serial
+
+BASEPATH = os.path.dirname(__file__)
+
+
+def main():
+    for name, workspace in generate_regions():
+        region.Region(name, workspace).dump(
+            os.path.join(BASEPATH, region.strip_cuts(name)),
+        )
+
+
+def generate_regions():
+
+    def standard(filename):
+        spec = serial.load_json_gz(os.path.join(BASEPATH, filename))
+        workspace = pyhf.workspace.Workspace(region.clear_poi(spec))
+        name, = workspace.channel_slices.keys()
+        return name, workspace
+
+    # rpc2l0b
+    yield standard("rpc2l0b_bkg.json.gz")
+
+    # rpc2l1b
+    yield standard("rpc2l1b_bkg.json.gz")
+
+    # rpc2l2b
+    yield standard("rpc2l2b_bkg.json.gz")
+
+    # rpc3lss1b
+    yield standard("rpc3lss1b_bkg.json.gz")
+
+    # rpv2l
+    yield standard("rpv2l_bkg.json.gz")
+
+
+if __name__ == "__main__":
+    main()

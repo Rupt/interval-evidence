74336b525afc39be3a1d338f9ddf2c1f192510b4
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-24 16:04:31 +0100

    improve attemot to silence printouts

--- pyhf_stuff/__init__.py
+++ pyhf_stuff/__init__.py
@@ -3,12 +3,11 @@ import os
 
 import jax
 import pyhf
+from pyhf.optimize.mixins import log as _pyhf_optimize_mixins_log
 
 # GPU might be powerful, but I want CPU portability
 jax.config.update("jax_platform_name", "cpu")
 jax.config.update("jax_enable_x64", True)
-
-pyhf.set_backend("jax")
 
 # https://github.com/google/jax/issues/6790
 # avoid internal threading to give the user control over multiprocessing
@@ -18,17 +17,23 @@ os.environ["XLA_FLAGS"] = " ".join(
 )
 
 
-# pyhf validation downloads schema files from the interweb every time objects
-# of certain classes are __init__ialized.
-# Not all of those classes offer options to disable that validation.
-# This has been a source of sporadic crashes, and I want to work without an
-# internet connection.
-def no_validate_no_network(*args, **kwargs):
+# pyhf
+
+pyhf.set_backend("jax")
+
+
+def _no_validate_no_network(*args, **kwargs):
+    # pyhf validation downloads schema files from the interweb every time
+    # objects of certain classes are __init__ialized.
+    # Not all of those classes offer options to disable that validation.
+    # This has been a source of sporadic crashes, and I want to work without an
+    # internet connection.
     ...
 
 
-pyhf.schema.validate = no_validate_no_network
+pyhf.schema.validate = _no_validate_no_network
 
 
 # pyhf screams into this logger when its fits fail
-logging.getLogger("mixins").setLevel(logging.CRITICAL)
+
+_pyhf_optimize_mixins_log.setLevel(logging.CRITICAL)

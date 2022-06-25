36c9d26a171dc273eb3528994a74c24c6a59ecb6
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-25 03:06:57 +0100

    add ss3l

---
+++ searches/ins1754675/dump_fit_mcmc.py
@@ -0,0 +1,60 @@
+"""
+time python searches/ins1754675/dump_fit_mcmc.py
+
+"""
+
+import os
+
+import numpy
+
+from pyhf_stuff import fit_mcmc_mix, mcmc_core, region
+
+BASEPATH = os.path.dirname(__file__)
+
+
+def main():
+    region_name_to_scan = {
+        "Rpv2L": (0, 15),
+        "Rpc3LSS1b": (0, 10),
+        "Rpc2L2b": (0, 20),
+        "Rpc2L1b": (0, 15),
+        "Rpc2L0b": (0, 15),
+    }
+
+    for name, (lo, hi) in region_name_to_scan.items():
+        print(name)
+        dump_region(name, lo, hi)
+
+
+def dump_region(name, lo, hi, nbins=50):
+    dir_region = os.path.join(BASEPATH, name)
+    region_1 = region.Region.load(dir_region)
+
+    dir_fit = os.path.join(dir_region, "fit")
+
+    mix = fit_mcmc_mix.fit(
+        region_1,
+        nbins,
+        (lo, hi),
+        seed=0,
+        nsamples=100_000,
+        nrepeats=100,
+    )
+    mix.dump(dir_fit)
+
+    neff = mcmc_core.n_by_fit(mix).sum()
+    nrepeats = mix.nrepeats
+    nsamples = mix.nsamples
+    total = numpy.sum(mix.yields)
+    print(
+        "acceptance: %.2f (%d / %d)"
+        % (total / (nrepeats * nsamples), total, nrepeats * nsamples)
+    )
+    print(
+        "efficiency: %.2f (%.1f / %.1f)"
+        % (nrepeats * neff / total, neff, total / nrepeats)
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ searches/ins1754675/dump_fits.py
@@ -0,0 +1,60 @@
+"""
+time python searches/ins1754675/dump_fits.py
+
+"""
+
+import os
+
+from pyhf_stuff import (
+    fit_cabinetry,
+    fit_cabinetry_post,
+    fit_linspace,
+    fit_normal,
+    region,
+)
+
+BASEPATH = os.path.dirname(__file__)
+
+
+def main():
+    region_name_to_scan = {
+        "Rpv2L": (0, 15),
+        "Rpc3LSS1b": (0, 10),
+        "Rpc2L2b": (0, 20),
+        "Rpc2L1b": (0, 15),
+        "Rpc2L0b": (0, 15),
+    }
+
+    for name, (lo, hi) in region_name_to_scan.items():
+        print(name)
+        dump(name, lo, hi)
+
+
+def dump(name, lo, hi, *, nbins=50, region_name_to_anchors=None):
+    if region_name_to_anchors is None:
+        region_name_to_anchors = {}
+
+    dir_region = os.path.join(BASEPATH, name)
+    region_1 = region.Region.load(dir_region)
+
+    dir_fit = os.path.join(dir_region, "fit")
+
+    # cabinetry
+    fit_cabinetry.fit(region_1).dump(dir_fit)
+    fit_cabinetry_post.fit(region_1).dump(dir_fit)
+
+    # normal
+    fit_normal.fit(region_1).dump(dir_fit)
+
+    # linspace
+    fit_linspace.fit(
+        region_1,
+        lo,
+        hi,
+        nbins + 1,
+        anchors=region_name_to_anchors.get(name),
+    ).dump(dir_fit)
+
+
+if __name__ == "__main__":
+    main()

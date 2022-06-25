237f5c094aba5462ce00b794a05e8b81241d0c22
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-25 02:48:14 +0100

    remove printout temp hack

--- pyhf_stuff/fit_mcmc_mix.py
+++ pyhf_stuff/fit_mcmc_mix.py
@@ -42,12 +42,6 @@ def fit(
 
     yields, errors = mcmc_core.summarize_hists(hists)
 
-    # TODO
-    neff = mcmc_core.n_by_variance(hists).sum()
-    total = numpy.sum(yields)
-    if nrepeats * neff / total < 0.05:
-        print(hists.tolist())
-
     return FitMcmcMix(
         # histogram arguments
         nbins=nbins,

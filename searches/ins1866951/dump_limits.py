e473d4fbabd4221ab0bed1b9f3f1a55d09447630
Rupert Tombs <rupert.tombs@gmail.com>
2022-06-30 15:02:59 +0100

    insert 3l limit configuration

--- searches/ins1866951/dump_limits.py
+++ searches/ins1866951/dump_limits.py
@@ -9,7 +9,6 @@ from pyhf_stuff import (
     fit_cabinetry,
     fit_linspace,
     fit_mcmc_mix,
-    fit_mcmc_tfp_ham,
     fit_normal,
     fit_signal,
     limit,
@@ -22,23 +21,26 @@ BASEPATH = os.path.dirname(__file__)
 
 def main():
     region_name_to_scan = {
-        "SR_E_mll_1": (0, 15),
-        "SR_E_mll_2": (0, 80),
-        "SR_E_mll_3": (0, 100),
-        "SR_E_mll_5": (0, 150),
-        "SR_E_mll_10": (0, 200),
-        "SR_E_mll_20": (0, 300),
-        "SR_E_mll_30": (0, 300),
-        "SR_E_mll_40": (0, 300),
-        "SR_E_mll_60": (0, 300),
-        "SR_S_100p5": (0, 60),
-        "SR_S_101": (0, 110),
-        "SR_S_102": (0, 100),
-        "SR_S_105": (0, 150),
-        "SR_S_110": (0, 150),
-        "SR_S_120": (0, 200),
-        "SR_S_130": (0, 200),
-        "SR_S_140": (0, 250),
+        "incSR_WZ_1": (0, 50),
+        "incSR_WZ_2": (0, 20),
+        "incSR_WZ_3": (0, 30),
+        "incSR_offWZ_highEt_nj_a": (0, 20),
+        "incSR_offWZ_highEt_nj_b": (0, 20),
+        "incSR_offWZ_highEt_nj_c1": (0, 30),
+        "incSR_offWZ_highEt_nj_c2": (0, 20),
+        "incSR_offWZ_lowEt_b": (0, 50),
+        "incSR_offWZ_highEt_b": (0, 20),
+        "incSR_offWZ_lowEt_c": (0, 100),
+        "incSR_offWZ_highEt_c": (0, 40),
+        "incSR_offWZ_d": (0, 150),
+        "incSR_offWZ_e1": (0, 200),
+        "incSR_offWZ_e2": (0, 200),
+        "incSR_offWZ_f1": (0, 200),
+        "incSR_offWZ_f2": (0, 150),
+        "incSR_offWZ_g1": (0, 250),
+        "incSR_offWZ_g2": (0, 200),
+        "incSR_offWZ_g3": (0, 150),
+        "incSR_offWZ_g4": (0, 120),
     }
 
     for name, (lo, hi) in region_name_to_scan.items():
@@ -79,10 +81,7 @@ def dump_region(name, lo, hi):
     dump(fit.filename, fit, models.linspace)
 
     # mcmc
-    if name.startswith("SR_E"):
-        fit = fit_mcmc_mix.FitMcmcMix.load(path_fit)
-    else:
-        fit = fit_mcmc_tfp_ham.FitMcmcTfpHam.load(path_fit)
+    fit = fit_mcmc_mix.FitMcmcMix.load(path_fit)
     dump(fit.filename, fit, models.mcmc)
 
     # fit signal scan

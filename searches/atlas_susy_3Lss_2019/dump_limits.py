eaadf65673931ce16411d6e7f2c6c026f0211a5a
Rupert Tombs <rupert.tombs@gmail.com>
2022-07-09 15:07:57 +0100

    update a few limit results with extra scans and 2.5 to 2

--- searches/atlas_susy_3Lss_2019/dump_limits.py
+++ searches/atlas_susy_3Lss_2019/dump_limits.py
@@ -7,6 +7,7 @@ import os
 
 from discohist import (
     fit_cabinetry,
+    fit_cabinetry_post,
     fit_linspace,
     fit_mcmc_mix,
     fit_normal,
@@ -56,10 +57,24 @@ def dump_region(name, lo, hi):
     fit = fit_cabinetry.FitCabinetry.load(path_fit)
     dump(fit.filename, fit, models.cabinetry)
 
+    fit = fit_cabinetry_post.FitCabinetryPost.load(path_fit)
+    dump(fit.filename, fit, models.cabinetry_post)
+
     # normal
     fit = fit_normal.FitNormal.load(path_fit)
     dump(fit.filename, fit, models.normal)
     dump(fit.filename + "_log", fit, models.normal_log)
+
+    # best fit only, no uncertainty
+    limit.dump_scan_delta(
+        "delta",
+        fit.yield_linear,
+        path_limit,
+        region_1.ndata,
+        lo,
+        hi,
+        print_=True,
+    )
 
     # linspace
     fit = fit_linspace.FitLinspace.load(path_fit)
@@ -71,7 +86,9 @@ def dump_region(name, lo, hi):
 
     # fit signal scan
     signal = fit_signal.FitSignal.load(path_fit)
-    limit.dump_scan_fit_signal(signal.filename, signal, path_limit)
+    limit.dump_scan_fit_signal(
+        signal.filename, signal, path_limit, print_=True
+    )
 
 
 if __name__ == "__main__":

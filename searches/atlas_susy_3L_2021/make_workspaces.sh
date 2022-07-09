b2455c8b4ed1c73c9223768fc327e42878465fdd
Rupert Tombs <rupert.tombs@gmail.com>
2022-07-09 19:38:08 +0100

    pull latest pyhf version (seems just different in folder)

--- searches/atlas_susy_3L_2021/make_workspaces.sh
+++ searches/atlas_susy_3L_2021/make_workspaces.sh
@@ -3,11 +3,11 @@
 # ./searches/atlas_susy_3L_2021/make_workspaces.sh
 cd $(dirname $0)
 
-curl -OJLH "Accept: application/x-tar" https://doi.org/10.17182/hepdata.95751.v1/r3
+curl -OJLH "Accept: application/x-tar" https://doi.org/10.17182/hepdata.95751.v2/r3
 
-tar -f statistical_models.tar.gz -x statistical_models/bkg_offshell.json statistical_models/bkg_onshell.json
+tar -f statistical_models.tar.gz -x bkg_offshell.json bkg_onshell.json
 
-python ../../discohist/specgz.py statistical_models/bkg_offshell.json offshell_bkg.json.gz
-python ../../discohist/specgz.py statistical_models/bkg_onshell.json onshell_bkg.json.gz
+python ../../discohist/specgz.py bkg_offshell.json offshell_bkg.json.gz
+python ../../discohist/specgz.py bkg_onshell.json onshell_bkg.json.gz
 
-rm -r statistical_models.tar.gz statistical_models
+rm -r statistical_models.tar.gz bkg_onshell.json bkg_offshell.json
